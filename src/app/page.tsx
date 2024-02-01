d757c437d0ef1cf896a6fc4851f52dd134cf94e0
Arsalan <arsalan.akkhter@gmail.com>
2024-02-01 18:04:10 +0500

    Initial Push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ export default function Home() {
   return (
     <div className="max-w-4xl bg-red-300 mx-auto p-5">
       <h1 className="text-center text-xl">TODO</h1>
-      </div>
+    </div>
   );
 }
