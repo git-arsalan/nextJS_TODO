68a42d5b4431fab323989ba1f03cddecfb52f2c8
Arsalan <arsalan.akkhter@gmail.com>
2024-03-01 16:31:49 +0500

    03012024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <div className="max-w-4xl bg-red-300 mx-auto p-5">
       <h1 className="text-center text-xl">TODO</h1>
+      <div>
+        <input placeholder="Write Movie Name"></input>
+        <button>ADD</button>
+
+      </div>
     </div>
   );
 }
